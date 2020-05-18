408d442e17bc08bbc3a46b05f4ae3f6e6e149a8a
paskeviciute.rima@gmail.com <paskeviciute.rima@gmail.com>
2020-05-18 16:18:11 +0100

    show custom list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [todos, setTodos] = useState([
+  {
+    content: 'Pickup dry cleaning',
+    isCompleted: true,
+  },
+  {
+    content: 'Get haircut',
+    isCompleted: false,
+  },
+  {
+    content: 'Build a todo app in React',
+    isCompleted: false,
+  }
+])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+          <div className="todo">
+            <div className="checkbox" />
+            <input type="text" value={todo.content} />
+          </div>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
