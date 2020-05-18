4f17f27ce915dcb3123facb45f46053e0cb4856b
paskeviciute.rima@gmail.com <paskeviciute.rima@gmail.com>
2020-05-18 16:59:00 +0100

    add new todo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,31 @@ const [todos, setTodos] = useState([
     content: 'Build a todo app in React',
     isCompleted: false,
   }
-])
+]);
+
+function handleKeyDown(e, i) {
+  if (e.key === 'Enter') {
+    createTodoAtIndex(e, i);
+  }
+}
+
+function createTodoAtIndex(e, i) {
+  const newTodos = [...todos];
+  newTodos.splice(i + 1, 0, {
+    content: '',
+    isCompleted: false,
+  });
+  setTodos(newTodos);
+  setTimeout(() => {
+    document.forms[0].elements[i + 1].focus();
+  }, 0);
+}
+
+function updateTodoAtIndex(e, i) {
+  const newTodos = [...todos];
+  newTodos[i].content = e.target.value;
+  setTodos(newTodos)
+}
 
   return (
     <div className="app">
@@ -28,7 +52,11 @@ const [todos, setTodos] = useState([
           {todos.map((todo, i) => (
           <div className="todo">
             <div className="checkbox" />
-            <input type="text" value={todo.content} />
+            <input type="text"
+                   value={todo.content}
+                   onKeyDown={e => handleKeyDown(e, i)}
+                   onChange={e => updateTodoAtIndex(e, i)} />
+
           </div>
           ))}
         </ul>
